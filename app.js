8cd79df374d1b737bf1d42e4b520327f1b331e4b
00gigs <124551005+00gigs@users.noreply.github.com>
2023-11-12 15:05:15 -0500

    main

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+const cors = require('cors');
+
+app.use(cors())
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('form')
+})
+
+//submission
+app.post('/submit',(req,res)=>{
+    console.log('Form data received:', req.body)
+
+    res.send('Form submitted successfully!')
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`)
+})
+
+
+
+
+
+
+
